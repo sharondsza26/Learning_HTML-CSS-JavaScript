8e1907a9f014ada7a322030a41e04354a6bac2d7
sharondsza26 <sharondsouza26@gmail.com>
2020-04-07 01:23:23 +0530

    Learning to use actions in jQuery

---
+++ jQuery/actionsPractice/actionsPractice.js
@@ -0,0 +1,14 @@
+// Guided Practice
+$(".replyYes").click(function() {  
+    $("h1").text("Yes");
+      
+    
+});
+
+//Independent Practice
+$(".replyNo").click(function() {  
+      $("h1").text("stay away from my donut!");
+      $("body").css("background-color", "red");
+    
+});
+
