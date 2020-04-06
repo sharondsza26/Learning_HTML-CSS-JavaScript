503d04ccef96e6fa5851ba1431a1c42cf0c7f53c
sharondsza26 <sharondsouza26@gmail.com>
2020-04-07 01:29:12 +0530

    simple Game in jQuery 1/3

---
+++ jQuery/Whack-A-Mole/Whack-A-Mole.js
@@ -0,0 +1,17 @@
+$(".yard1").click(function(){
+    $(".mole1").hide();
+});
+    
+$(".yard2").click(function(){
+    $(".moleTwo").hide();
+});
+    
+$(".yard3").click(function(){
+    $(".thirdMole").hide();
+});
+    
+$(".reset").click(function(){
+    $(".mole1").show();
+    $(".moleTwo").show();
+    $(".thirdMole").show();
+});
