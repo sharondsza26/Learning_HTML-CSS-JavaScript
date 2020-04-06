337a1681ccba61263c58d05c799486a6198202e0
sharondsza26 <sharondsouza26@gmail.com>
2020-04-07 01:26:56 +0530

    simple jQuery assignment

---
+++ jQuery/funHouse/funHouse.js
@@ -0,0 +1,13 @@
+$(".garage-door-opener").click(function() {
+    $(".garage-door").slideToggle();
+});
+
+$(".pokeball").click(function() {
+    $(".pikachu").fadeToggle();
+    
+});
+
+$(".light-switch").click(function() {
+    $(".light-bulb").toggle();
+
+});
