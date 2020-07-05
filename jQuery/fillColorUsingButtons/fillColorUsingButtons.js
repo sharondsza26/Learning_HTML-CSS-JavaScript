8dfedadd6ce6f3ccb5bfadd1479437b06ab4d11c
sharondsza26 <sharondsouza26@gmail.com>
2020-07-05 17:28:37 +0530

    Learning to use fill in jquery

---
+++ jQuery/fillColorUsingButtons/fillColorUsingButtons.js
@@ -0,0 +1,7 @@
+// Write your javascript code here
+$("#set-odd-button").click(function(){
+    $(".odd").css("background-color","blue");
+});
+$("#set-even-button").click(function(){
+    $(".even").css("background-color","red");
+});
