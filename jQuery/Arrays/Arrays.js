241a1f8021d39efe1462f285018e71d5f3de61cf
sharondsza26 <sharondsouza26@gmail.com>
2020-07-05 17:21:08 +0530

    learning arrays in jQuery

---
+++ jQuery/Arrays/Arrays.js
@@ -0,0 +1,43 @@
+/*
+	PART 1 
+ */
+
+// Task 1: Update the variable songList so that it is an array with at least 3 song titles.
+let songList = ["song1", "song2", "song3","song4"];
+console.log();
+
+// Task 2: Update the variable classList so that it is an array with 4 Code Nation students (including yourself) in your class.
+let classList = ["stud1","stud2","stud3","stud4"];
+console.log();
+
+// Task 3: Update the variable teacherList so that it is an array with all the Code Nation adults in your class.
+let teacherList = ["teach1","teach2", "teach3","teach4"];
+console.log();
+
+// Task 4: Update the variable familyAges so that it is an array with a list of ages (numbers) of your family members.
+let familyAges=[20,30,40,50];
+console.log();
+
+//PAUSE HERE
+
+/*
+	PART 2 
+ */
+
+// Task 5: Update the variable favSong to retrieve the array element of your favorite song in the songList array (Task 1). 
+let favSong= songList[0];
+console.log();
+
+// Task 6: Update the variable bestStudent to retrieve your name from the classList array (Task 2).
+let bestStudent = classList[1];
+console.log();
+
+// Task 7: Update the variable tallestTeacher to retrieve the array element of the tallest teacher in the teacherList array (Task 3).
+let tallestTeacher =teacherList[2];
+console.log();
+
+// Task 8: Update the variable oldestMemeber to retrieve the array element of the oldest member of your family in the familyAges array. (Task 4)
+let oldestMember =familyAges[3];
+console.log();
+
+
